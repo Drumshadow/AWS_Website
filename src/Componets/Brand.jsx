bf0cd15976c705b58fac8cbaf4cb38c550709dc3
Ashton Stephen Damrel <stephendamrel@gmail.com>
2020-01-24 19:24:28 -0600

    no message

--- src/Componets/Brand.jsx
+++ src/Componets/Brand.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from "styled-components";
 
-import logo from "../assets/logo-min.png";
+import logo from "../assets/baylor.png";
 
 const Brand = () => {
     return (
